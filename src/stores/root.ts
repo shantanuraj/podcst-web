2419d4dfdbd8a4a2aec0a02bfb6cb4dde2ff8c47
Shantanu Raj <s@sraj.me>
2017-09-02 21:27:43 +0530

    Remove extraneous states

--- src/stores/root.ts
+++ src/stores/root.ts
@@ -11,73 +11,35 @@ import {
 } from 'redux';
 
 import {
-  AuthActions,
-  AuthState,
-  auth,
-  fetchAuthEpic,
-  authFulfilledEpic,
-} from './auth';
-
-import {
   RouterActions,
   RouterState,
   router,
   routerEpic,
 } from './router';
 
-import {
-  TextsActions,
-  TextsState,
-  texts,
-  fetchTextsEpic,
-  searchTextsEpic,
-} from './texts';
-
 /**
  * Combined application actions interface
  */
 export type Actions =
-  AuthActions |
-  RouterActions |
-  TextsActions;
+  RouterActions;
 
 /**
  * Combined application state interface
  */
 export interface State {
-  auth: AuthState;
   router: RouterState;
-  texts: TextsState;
 };
 
 export const getDefaultState = (): State => ({
-  auth: {
-    authorized: false,
-    code: '',
-    host: '',
-  },
   router: {
     path: '/',
   },
-  texts: {
-    loading: false,
-    texts: [],
-    threads: [],
-    filteredThreads: [],
-    query: '',
-  }
 });
 
 export const rootEpic = combineEpics<Actions, State>(
-  authFulfilledEpic,
-  fetchAuthEpic,
-  fetchTextsEpic,
   routerEpic,
-  searchTextsEpic,
 );
 
 export const rootReducer = combineReducers<State>({
-  auth,
   router,
-  texts,
 });
