ff8066846c947cbedfaa8b8c5544665ad6693d02
Shantanu Raj <s@sraj.me>
2017-09-04 11:14:33 +0530

    Toggle player view when audio player is initialized

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -7,8 +7,29 @@ import {
 } from 'preact';
 
 import {
+  style,
+} from 'typestyle';
+
+import {
   PlayerState,
 } from '../stores/player';
+
+const player = style({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'flex-start',
+  backgroundColor: '#292929',
+  position: 'fixed',
+  bottom: 0,
+  left: 0,
+  height: '64px',
+  width: '100%',
+  zIndex: 500,
+  paddingLeft: 16,
+  fontSize: 20,
+  color: 'white',
+  boxShadow: `0px 4px 32px 4px rgba(0,0,0,0.75)`,
+});
 
 interface PlayerProps extends PlayerState {
   pause: () => void;
@@ -17,8 +38,12 @@ interface PlayerProps extends PlayerState {
   skipToPrev: () => void;
 }
 
-const Player = (_props: PlayerProps) => (
-  <div />
+const Player = ({
+  state,
+}: PlayerProps) => (
+  state === 'stopped' ?
+    null :
+    <div class={player} />
 );
 
 export default Player;
