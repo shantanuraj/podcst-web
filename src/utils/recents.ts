872f7ed3d685ec8e4cbe8a5cd4a15b66d43f2965
Shantanu Raj <s@sraj.me>
2017-11-29 19:06:45 +0530

    Limit number of recent episodes to 50

--- src/utils/recents.ts
+++ src/utils/recents.ts
@@ -8,13 +8,17 @@
 const FEED_EPISODES_LIMIT = 4;
 
 /**
+ * Total limit on recent episodes
+ */
+const EPISODES_LIMIT = 50;
+
+/**
  * Returns list of episodes sorted by publish date in descending order
  * @param subs - User Subcriptions
  */
-export const recents = (subs: ISubscriptionsMap): App.IEpisodeInfo[] => {
-  const episodes = Object.keys(subs)
+export const recents = (subs: ISubscriptionsMap): App.IEpisodeInfo[] =>
+  Object.keys(subs)
     .map(feed => subs[feed].episodes.slice(0, FEED_EPISODES_LIMIT))
-    .reduce((acc, feedEpisodes) => [...acc, ...feedEpisodes], []);
-
-  return episodes.slice().sort((a, b) => (b.published || 0) - (a.published || 0));
-};
+    .reduce((acc, feedEpisodes) => [...acc, ...feedEpisodes], [])
+    .sort((a, b) => (b.published || 0) - (a.published || 0))
+    .slice(0, EPISODES_LIMIT);
