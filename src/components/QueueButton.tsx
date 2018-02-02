6df196093e3a5bab0e5263a02d46a48abedf0b47
Swapnil Raj <raj.swapnil99@gmail.com>
2018-02-02 20:45:56 +0000

    Refactor QueueButton, temporarily use info button icon

--- src/components/QueueButton.tsx
+++ src/components/QueueButton.tsx
@@ -4,38 +4,18 @@
 
 import { h } from 'preact';
 
-import { style } from 'typestyle';
-
-const queueButton = (theme: App.ITheme) =>
-  style({
-    display: 'inline-block',
-    minWidth: '80px',
-    borderRadius: '3px',
-    padding: '8px',
-    background: 'transparent',
-    color: theme.text,
-    border: `2px solid ${theme.accent}`,
-    $nest: {
-      '&:hover, &:focus, &:active, &[data-is-playing], &[data-is-paused]': {
-        outline: 0,
-        backgroundColor: theme.accent,
-        color: theme.background,
-      },
-    },
-  });
+import Icon from '../svg/Icon';
 
 interface IQueueButtonProps {
   episode: App.IEpisodeInfo;
   theme: App.ITheme;
-  add(episode: App.IEpisodeInfo);
+  add: (episode: App.IEpisodeInfo) => void;
 }
 
-const QueueButton = ({ episode, theme, add }: IQueueButtonProps) => {
-  return (
-    <button class={queueButton(theme)} onClick={add(episode)}>
-      Add
-    </button>
-  );
-};
+const QueueButton = ({ episode, theme, add }: IQueueButtonProps) => (
+  <div onClick={() => add(episode)}>
+    <Icon icon={'info'} color={theme.accent} />
+  </div>
+);
 
 export default QueueButton;
