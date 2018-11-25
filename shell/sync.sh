3b99761b110aaca6ca8847c6e4d247d692afe85b
Shantanu Raj <s@sraj.me>
2018-11-25 18:26:30 +0530

    Make sync script more generic

--- shell/sync.sh
+++ shell/sync.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+# Fallback if bucket not specified
+BUCKET=${BUCKET_URL-static.podcst.io}
+
 # Delete existing folder on aws
-aws s3 rm "s3://static.podcst.io/$1/"
+aws s3 rm "s3://$BUCKET/$1/"
 
 # Sync dist to AWS
-aws s3 sync dist "s3://static.podcst.io/$1/"
+aws s3 sync dist "s3://$BUCKET/$1/"
 
 # Echo success
-echo "Synced dist in $1 mode"
+echo "Synced dist to $1"
