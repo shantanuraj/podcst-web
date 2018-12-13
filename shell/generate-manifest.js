6fcb066cde0bad5cc934459572a0d9e48cb33b30
Shantanu Raj <s@sraj.me>
2018-12-13 14:05:51 +0530

    Experiment with different start_rul in manifest

--- shell/generate-manifest.js
+++ shell/generate-manifest.js
@@ -7,33 +7,34 @@ const outPath = join(outDir, 'manifest.json');
 
 const appName = process.env.PODCST_APP || 'Podcst';
 const baseUrl = process.env.PODCST_URL || 'play.podcst.app';
+const cdnUrl = baseUrl.replace('play', 'static');
 
 const templateManifest = {
   short_name: appName,
   name: appName,
   icons: [
     {
-      src: `https://${baseUrl}/icons/launcher-72.png`,
+      src: `https://${cdnUrl}/icons/launcher-72.png`,
       type: 'image/png',
       sizes: '72x72'
     },
     {
-      src: `https://${baseUrl}/icons/launcher-96.png`,
+      src: `https://${cdnUrl}/icons/launcher-96.png`,
       type: 'image/png',
       sizes: '96x96'
     },
     {
-      src: `https://${baseUrl}/icons/launcher-144.png`,
+      src: `https://${cdnUrl}/icons/launcher-144.png`,
       type: 'image/png',
       sizes: '144x144'
     },
     {
-      src: `https://${baseUrl}/icons/launcher-192.png`,
+      src: `https://${cdnUrl}/icons/launcher-192.png`,
       type: 'image/png',
       sizes: '192x192'
     },
     {
-      src: `https://${baseUrl}/icons/launcher-512.png`,
+      src: `https://${cdnUrl}/icons/launcher-512.png`,
       type: 'image/png',
       sizes: '512x512'
     }
@@ -41,7 +42,7 @@ const templateManifest = {
   background_color: '#212121',
   theme_color: '#212121',
   display: 'standalone',
-  start_url: '/feed/top'
+  start_url: '/'
 };
 
 module.exports = function generateManifest() {
