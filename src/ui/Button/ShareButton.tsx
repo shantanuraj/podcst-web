62ee0f5e007073e817d6558476b7d963bd0bc553
Shantanu Raj <s@sraj.me>
2020-11-01 22:38:15 +0530

    feat(ui): Add share button

---
+++ src/ui/Button/ShareButton.tsx
@@ -0,0 +1,24 @@
+import { forwardRef, memo } from 'react';
+import { IShareEnabledNavigator } from '../../types';
+import { Button, ButtonProps } from './Button';
+
+type ShareButtonProps = ButtonProps & {
+  text: string;
+  title: string;
+  url: string;
+};
+
+const share = ({ text, title, url }: ShareButtonProps) => {
+  if ('share' in navigator) {
+    (navigator as IShareEnabledNavigator)
+      .share({ text, title, url })
+      .catch((err) => console.error('Error sharing', err));
+  }
+};
+
+export const ShareButton = memo(
+  forwardRef<HTMLButtonElement, ShareButtonProps>(function ShareButton(props, ref) {
+    if (!('share' in navigator)) return null;
+    return <Button {...(props as ButtonProps)} ref={ref} onClick={() => share(props)} />;
+  }),
+);
