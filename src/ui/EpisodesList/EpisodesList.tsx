2dbdfd7dd41792673f9a104f1dfaf89cdfd9bb86
Shantanu Raj <s@sraj.me>
2020-11-02 03:09:59 +0530

    feat(ui): Use virtual list for episodes page

--- src/ui/EpisodesList/EpisodesList.tsx
+++ src/ui/EpisodesList/EpisodesList.tsx
@@ -1,3 +1,5 @@
+import { useRef } from 'react';
+import { useVirtual } from 'react-virtual';
 import { IEpisodeInfo } from '../../types';
 import { EpisodeItem } from './EpisodeItem';
 
@@ -8,13 +10,33 @@ type EpisodesListProps = {
 };
 
 export function EpisodesList({ episodes }: EpisodesListProps) {
+  const containerRef = useRef<HTMLDivElement | null>(null);
+  const { totalSize, virtualItems } = useVirtual<HTMLDivElement>({
+    size: episodes.length,
+    parentRef: containerRef,
+    estimateSize: getRowHeight,
+    overscan: 5,
+  });
+
   return (
-    <ul className={styles.list}>
-      {episodes.map((episode, idx) => (
-        <li key={`${idx}-${episode.title}`}>
-          <EpisodeItem episode={episode} />
-        </li>
-      ))}
-    </ul>
+    <div className={styles.container} ref={containerRef}>
+      <ul className={styles.list} style={{ height: `${totalSize}px` }}>
+        {virtualItems.map(({ index, start }) => {
+          const episode = episodes[index];
+          return (
+            <li
+              key={`${index}-${episode.title}`}
+              style={{
+                transform: `translateY(${start}px)`,
+              }}
+            >
+              <EpisodeItem episode={episode} />
+            </li>
+          );
+        })}
+      </ul>
+    </div>
   );
 }
+
+const getRowHeight = () => 84;
