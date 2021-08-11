01c5b23ada154ae71d4e685f2bba99e70f5240b3
Shantanu Raj <s@sraj.me>
2021-08-11 17:27:42 +0200

    feat(shortcuts): Add search shortcut

--- src/ui/Search/Search.tsx
+++ src/ui/Search/Search.tsx
@@ -1,9 +1,23 @@
 import * as React from 'react';
+import { useKeydown } from '../../shared/keyboard/useKeydown';
 
 import styles from './Search.module.css';
 
 export function Search() {
+  const searchRef = React.useRef<HTMLInputElement>(null);
+  const focusSearchShortcut = React.useCallback((e: KeyboardEvent) => {
+    if (e.metaKey && e.key === 'k') {
+      searchRef.current?.focus();
+    }
+  }, []);
+  useKeydown(focusSearchShortcut);
   return (
-    <input className={styles.input} aria-label="Search podcasts" type="text" placeholder="Search" />
+    <input
+      className={styles.input}
+      aria-label="Search podcasts"
+      type="text"
+      placeholder="Search"
+      ref={searchRef}
+    />
   );
 }
