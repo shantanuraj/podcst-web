1a98075bf926d8e685051c8bc43485767226c6e8
Shantanu Raj <s@sraj.me>
2021-08-12 01:00:58 +0200

    feat(player): Sync to browser media session

--- src/shared/player/PlayerProvider.tsx
+++ src/shared/player/PlayerProvider.tsx
@@ -1,10 +1,13 @@
 import * as React from 'react';
+import { IEpisodeInfo } from '../../types';
 import AudioUtils from './AudioUtils';
 import { PlayerActionsContext, PlayerStateContext, setPlayerState } from './context';
+import { updateMetadata } from './mediaUtils';
 import { initialState, playerReducer } from './store';
 
 export const PlayerProvider: React.FC = ({ children }) => {
   const [value, dispatch] = React.useReducer(playerReducer, initialState);
+  const currentEpisodeRef = React.useRef<IEpisodeInfo | null>(null);
 
   React.useEffect(() => {
     AudioUtils.init({
@@ -15,6 +18,18 @@ export const PlayerProvider: React.FC = ({ children }) => {
       seekUpdate: () => {},
     });
   }, []);
+
+  React.useEffect(() => {
+    const currentEpisode: IEpisodeInfo | undefined = value.queue[value.currentTrackIndex];
+    if (!currentEpisode) return;
+    if (currentEpisode.guid === currentEpisodeRef.current?.guid) {
+      return;
+    }
+
+    updateMetadata(currentEpisode, null, dispatch);
+
+    currentEpisodeRef.current = currentEpisode;
+  }, [value.queue, value.currentTrackIndex]);
 
   return (
     <PlayerActionsContext.Provider value={dispatch}>
