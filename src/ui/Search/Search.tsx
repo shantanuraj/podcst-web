fe33fa37a000bbe03aa5c23884997d46ab8814b3
Shantanu Raj <s@sraj.me>
2021-08-13 00:45:49 +0200

    feat(search): Implement search results

--- src/ui/Search/Search.tsx
+++ src/ui/Search/Search.tsx
@@ -1,9 +1,12 @@
 /// <reference types="react/next" />
 
+import { useCombobox, UseComboboxStateChange } from 'downshift';
+import router from 'next/router';
 import * as React from 'react';
 import { useSearch } from '../../data/search';
 import { shortcuts } from '../../shared/keyboard/shortcuts';
 import { useKeydown } from '../../shared/keyboard/useKeydown';
+import { IPodcastSearchResult } from '../../types';
 
 import styles from './Search.module.css';
 
@@ -15,28 +18,68 @@ export function Search() {
     deferConfig,
   );
 
-  const results = useSearch(term);
   const searchRef = React.useRef<HTMLInputElement>(null);
   const focusSearchShortcut = React.useCallback(() => {
     searchRef.current?.focus();
   }, []);
-  const handleInputChange = React.useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
-    setTerm(event.target.value);
-  }, []);
   useKeydown(shortcuts.search, focusSearchShortcut);
-  React.useEffect(() => {
-    console.log(results);
-  }, [results]);
+
+  const response = useSearch(term);
+  const searchResults = response.data || emptyResult;
+
+  const onInputValueChange = React.useCallback(
+    (changes: UseComboboxStateChange<IPodcastSearchResult>) => {
+      setTerm(changes.inputValue || '');
+    },
+    [],
+  );
+
+  const { isOpen, getMenuProps, getInputProps, getComboboxProps, highlightedIndex, getItemProps } =
+    useCombobox({
+      items: searchResults,
+      onInputValueChange,
+      itemToString: serealizeSearchResult,
+      onSelectedItemChange: onSelectionChange,
+    });
+
   return (
-    <input
-      className={styles.input}
-      aria-label="Search podcasts"
-      type="text"
-      placeholder="Search"
-      onChange={handleInputChange}
-      ref={searchRef}
-    />
+    <div {...getComboboxProps()} className={styles.search}>
+      <input
+        {...getInputProps({ ref: searchRef })}
+        // className={styles.input}
+        aria-label="Search podcasts"
+        type="text"
+        placeholder="Search"
+      />
+      <ul {...getMenuProps()} className={styles.results}>
+        {isOpen &&
+          searchResults.map((item, index) => (
+            <>
+              <li
+                className={styles.searchItem}
+                data-highlighted={highlightedIndex === index}
+                key={`${item}${index}`}
+                {...getItemProps({ item, index })}
+              >
+                <img loading="lazy" alt={`${item.title} by ${item.author}`} src={item.thumbnail} />
+                <div>
+                  <p className={styles.title}>{item.title}</p>
+                  <p className={styles.author}>{item.author}</p>
+                </div>
+              </li>
+            </>
+          ))}
+      </ul>
+    </div>
   );
 }
 
 const deferConfig = { timeoutMs: 200 };
+const emptyResult: IPodcastSearchResult[] = [];
+const serealizeSearchResult = (item: IPodcastSearchResult | null) => {
+  return item?.title || '';
+};
+const onSelectionChange = (changes: UseComboboxStateChange<IPodcastSearchResult>) => {
+  if (!changes.selectedItem) return;
+  router.push(`/episodes?feed=${changes.selectedItem.feed}`);
+};
