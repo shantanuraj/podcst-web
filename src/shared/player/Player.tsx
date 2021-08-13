5de467d02511adaf544b91233d7f13dc2f53161e
Shantanu Raj <s@sraj.me>
2021-08-13 02:56:19 +0200

    feat(player): Implement player

--- src/shared/player/Player.tsx
+++ src/shared/player/Player.tsx
@@ -1,7 +1,64 @@
+import { useCallback } from 'react';
+
+import { Icon } from '../../ui/icons/svg/Icon';
+
+import { usePlayerState } from './usePlayerState';
+import styles from './Player.module.css';
+import { usePlayerActions } from './usePlayerActions';
+import { resumeEpisode, seekBackward, seekForward, setPlayerState } from './context';
+
 export const Player = () => {
+  const { queue, currentTrackIndex, state } = usePlayerState();
+  const dispatch = usePlayerActions();
+
+  const currentEpisode = queue[currentTrackIndex];
+  const open = !!currentEpisode && state !== 'idle';
+
+  const pause = useCallback(() => {
+    dispatch(setPlayerState('paused'));
+  }, []);
+  const resume = useCallback(() => {
+    dispatch(resumeEpisode(currentEpisode));
+  }, []);
+  const seekBack = useCallback(() => {
+    dispatch(seekBackward());
+  }, []);
+  const seekAhead = useCallback(() => {
+    dispatch(seekForward());
+  }, []);
+
   return (
-    <div>
-      <h1>Player</h1>
+    <div className={styles.container} data-open={open}>
+      {currentEpisode && (
+        <div className={styles.player}>
+          <img
+            alt={`${currentEpisode.title} by ${currentEpisode.author}`}
+            src={currentEpisode.cover}
+          />
+          <div className={styles.controls}>
+            <button onClick={seekBack}>
+              <Icon icon="seek-back" />
+            </button>
+            {state === 'playing' && (
+              <button onClick={pause}>
+                <Icon icon="pause" />
+              </button>
+            )}
+            {state === 'paused' && (
+              <button onClick={resume}>
+                <Icon icon="play" />
+              </button>
+            )}
+            <button onClick={seekAhead}>
+              <Icon icon="seek-forward" />
+            </button>
+          </div>
+          <div className={styles.info}>
+            <p className={styles.title}>{currentEpisode.title}</p>
+            <p className={styles.author}>{currentEpisode.author}</p>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
