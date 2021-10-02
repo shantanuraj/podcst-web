9df52a41073818119d2df03f6cc5573ef47bfa7c
Shantanu Raj <s@sraj.me>
2021-10-02 20:22:14 +0200

    feat(code): Reuse PlayButton implementation
    - Allow support for children consuming isCurrentEpisode and isPlaying
      properties

--- src/ui/Button/PlayButton.tsx
+++ src/ui/Button/PlayButton.tsx
@@ -9,12 +9,16 @@ import {
 import { IEpisodeInfo } from '../../types';
 import { Button, ButtonProps } from './Button';
 
-interface PlayButtonProps extends ButtonProps {
+export interface PlayButtonProps extends ButtonProps {
   episode: IEpisodeInfo;
-};
+  children?: (props: { isCurrentEpisode: boolean; isPlaying: boolean }) => JSX.Element;
+}
 
 export const PlayButton = memo(
-  forwardRef<HTMLButtonElement, PlayButtonProps>(function PlayButton({ episode, ...props }, ref) {
+  forwardRef<HTMLButtonElement, PlayButtonProps>(function PlayButton(
+    { episode, children = PlayButtonContent, ...props },
+    ref,
+  ) {
     const state = usePlayer(getPlaybackState);
     const isCurrentEpisode = usePlayer(useCallback(selectIsCurrentEpisode(episode), [episode]));
     const isPlaying = isCurrentEpisode && (state === 'playing' || state === 'buffering');
@@ -38,11 +42,16 @@ export const PlayButton = memo(
 
     return (
       <Button {...props} ref={ref} onClick={handleClick} data-is-current={isCurrentEpisode}>
-        {isCurrentEpisode ? (isPlaying ? 'Pause' : 'Resume') : 'Play'}
+        {children({ isCurrentEpisode, isPlaying })}
       </Button>
     );
   }),
 );
+
+const PlayButtonContent = (props: { isCurrentEpisode: boolean; isPlaying: boolean }) => {
+  const { isCurrentEpisode, isPlaying } = props;
+  return isCurrentEpisode ? (isPlaying ? 'Pause' : 'Resume') : 'Play';
+};
 
 const selectIsCurrentEpisode = (episode: IEpisodeInfo) => (playerState: IPlayerState) =>
   getCurrentEpisode(playerState)?.guid === episode.guid && playerState.state !== 'idle';
