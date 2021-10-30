4c75aa5089cf72e9863d9146c854cab70f4569c3
Shantanu Raj <s@sraj.me>
2021-10-30 22:03:01 +0200

    feat(shortcuts): Implement new shortcuts
    - Remove drawer shortcut
    - Drawer isn't visible on desktop anymore
    
    - Add support for secondary shortcuts
    - Multiple shortcut combinations can be specfied for a key
    - Only primary action is shown to user
    
    - Implement new player shortcuts
    - Add support for going to shortcuts page
    - Add shortcut to go to queue
    
    Closes #61

--- src/shared/keyboard/shortcuts.ts
+++ src/shared/keyboard/shortcuts.ts
@@ -1,99 +1,181 @@
-export interface IShortcutInfo {
-  title: string;
-  key: string;
-  displayKey: string;
-  metaKey: boolean;
+export interface IKeyboardShortcut {
+    key: string;
+    metaKey: boolean;
+    shiftKey: boolean;
 }
 
-export const shortcuts = {
+export interface IShortcutInfo extends IKeyboardShortcut {
+  title: string;
+  displayKey: string;
+  secondary?: IKeyboardShortcut[];
+}
+
+type DisplayableShortcuts =
+  | 'home'
+  | 'subscriptions'
+  | 'recents'
+  | 'settings'
+  | 'search'
+  | 'theme'
+  | 'info'
+  | 'queue'
+  | 'togglePlayback'
+  | 'seekBack'
+  | 'seekAhead'
+  | 'seekTo'
+  | 'nextEpisode'
+  | 'previousEpisode'
+  | 'bumpRate'
+  | 'decreaseRate'
+  | 'mute'
+  | 'shortcuts';
+
+export const shortcuts: Record<DisplayableShortcuts, IShortcutInfo> = {
   home: {
     title: 'Home / Top',
     key: 'h',
     displayKey: 'h',
     metaKey: false,
+    shiftKey: false,
   },
   subscriptions: {
     title: 'Subscriptions',
     key: 's',
     displayKey: 's',
     metaKey: false,
+    shiftKey: false,
   },
   recents: {
     title: 'Recents',
     key: 'r',
     displayKey: 'r',
     metaKey: false,
+    shiftKey: false,
   },
   settings: {
     title: 'Settings',
     key: ',',
     displayKey: ',',
     metaKey: false,
-  },
-  drawer: {
-    title: 'Toggle drawer',
-    key: 'd',
-    displayKey: 'd',
-    metaKey: false,
+    shiftKey: false,
   },
   search: {
     title: 'Search',
     key: '/',
     displayKey: '/',
     metaKey: false,
+    shiftKey: false,
   },
   theme: {
     title: 'Toggle theme',
     key: 't',
     displayKey: 't',
     metaKey: false,
+    shiftKey: false,
   },
   info: {
     title: 'Show episode info',
     key: 'i',
     displayKey: 'i',
     metaKey: false,
+    shiftKey: false,
+  },
+  queue: {
+    title: 'Queue',
+    key: 'q',
+    displayKey: 'q',
+    metaKey: false,
+    shiftKey: false,
   },
   togglePlayback: {
     title: 'Play / Pause',
-    key: ' ',
-    displayKey: 'space',
+    key: 'k',
+    displayKey: 'k',
     metaKey: false,
+    shiftKey: false,
+    secondary: [
+      {
+        key: ' ',
+        metaKey: false,
+        shiftKey: false,
+      },
+    ],
   },
   seekBack: {
     title: 'Seek back',
-    key: 'ArrowLeft',
-    displayKey: '←',
+    key: 'j',
+    displayKey: 'j',
     metaKey: false,
+    shiftKey: false,
+    secondary: [
+      {
+        key: 'ArrowLeft',
+        metaKey: false,
+        shiftKey: false,
+      },
+    ],
   },
   seekAhead: {
     title: 'Seek ahead',
-    key: 'ArrowRight',
-    displayKey: '→',
+    key: 'l',
+    displayKey: 'l',
     metaKey: false,
+    shiftKey: false,
+    secondary: [
+      {
+        key: 'ArrowRight',
+        metaKey: false,
+        shiftKey: false,
+      },
+    ],
   },
   seekTo: {
     title: 'Seek to n %',
     key: '*',
     displayKey: '0-9',
     metaKey: false,
+    shiftKey: false,
+  },
+  nextEpisode: {
+    title: 'Next episode',
+    key: 'N',
+    displayKey: 'shift + n',
+    metaKey: false,
+    shiftKey: true,
+  },
+  previousEpisode: {
+    title: 'Previous episode',
+    key: 'P',
+    displayKey: 'shift + p',
+    metaKey: false,
+    shiftKey: true,
   },
   bumpRate: {
     title: 'Increase playback rate',
     key: '>',
     displayKey: '>',
     metaKey: false,
+    shiftKey: false,
   },
   decreaseRate: {
     title: 'Lower playback rate',
     key: '<',
     displayKey: '<',
     metaKey: false,
+    shiftKey: false,
   },
-  closeDrawer: {
-    title: 'Close drawer',
-    key: 'Escape',
-    displayKey: 'esc',
+  mute: {
+    title: 'Toggle mute',
+    key: 'm',
+    displayKey: 'm',
     metaKey: false,
+    shiftKey: false,
   },
-} as const;
+  shortcuts: {
+    title: 'Show shortcuts',
+    key: '?',
+    displayKey: 'shift + ?',
+    metaKey: false,
+    shiftKey: true,
+  },
+};
