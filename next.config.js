1a76d9291865340f4aa047221ce3c44f74c60d44
Shantanu Raj <s@sraj.me>
2023-07-01 08:33:03 +0200

    chore(api): Blanket enable cors

--- next.config.js
+++ next.config.js
@@ -24,6 +24,19 @@ module.exports = {
     esmExternals: false,
     legacyBrowsers: false,
   },
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      }
+    ]
+  },
   async redirects() {
     return [
       {
