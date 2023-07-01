6212715967f61f15d735a947aecf20af70fbcd27
Shantanu Raj <s@sraj.me>
2023-07-01 09:49:01 +0200

    fix(ui): Make podcast tiles style static

--- src/ui/PodcastTile/PodcastTile.tsx
+++ src/ui/PodcastTile/PodcastTile.tsx
@@ -14,25 +14,11 @@ export function PodcastTile({ podcast }: PodcastTileProps) {
   return (
     <Link href={`/episodes/${encodeURIComponent(feed)}`} className={styles.wrapper}>
       <div
-        className={styles.tile}
+        data-tile
         style={{
-          background: `linear-gradient(
-          to bottom,
-          rgba(0, 0, 0, 0) 0%,
-          rgba(0, 0, 0, 0) 12%,
-          rgba(0, 0, 0, 0) 23%,
-          rgba(0, 0, 0, 0) 34%,
-          rgba(0, 0, 0, 0) 50%,
-          rgba(0, 0, 0, 0.2) 60%,
-          rgba(0, 0, 0, 0.4) 76%,
-          rgba(17, 17, 17, 0.5) 88%,
-          rgba(28, 28, 28, 0.5) 94%,
-          rgba(43, 43, 43, 0.6) 95%,
-          rgba(19, 19, 19, 0.8) 100%
-        ),
-        no-repeat url(${cover})`,
-          backgroundSize: 'cover',
+          ['--asset-tile' as any]: `url(${cover})`,
         }}
+        className={styles.tile}
       >
         {/* <Image src={cover} alt={`${title} by ${author}`} height={200} width={200} /> */}
         <div>
