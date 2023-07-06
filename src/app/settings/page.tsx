ae396dcb6b6871a6dae5c3d590076c946119088b
Shantanu Raj <s@sraj.me>
2023-07-06 03:52:16 +0200

    fix(settings): Spacing

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -9,7 +9,7 @@ const SettingsPage = () => {
       <Link href="/settings/theme">Change Theme</Link>
       <Link href="/settings/shortcuts">Shortcuts</Link>
       <Link href="/settings/export">Export</Link>
-      <Link href="#about">Version:{process.env.appVersion}</Link>
+      <Link href="#version">Version: {process.env.appVersion}</Link>
     </nav>
   );
 };
