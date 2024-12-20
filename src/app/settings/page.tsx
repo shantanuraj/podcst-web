8aa3de904c38e337b60651a599f00fc194b31ffa
Shantanu Raj <s@sraj.me>
2024-12-20 23:23:30 +0100

    feat(settings): Add author info

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -10,6 +10,16 @@ const SettingsPage = () => {
       <Link href="/settings/shortcuts">Shortcuts</Link>
       <Link href="/settings/export">Export</Link>
       <Link href="#version">Version: {process.env.appVersion}</Link>
+      <div className={styles.footer}>
+        <span> © {new Date().getFullYear()}</span>
+
+        <div>
+          <span>Made by </span>
+          <Link href="https://sraj.me" target="_blank" rel="noopener noreferrer">
+            Shantanu Raj
+          </Link>
+        </div>
+      </div>
     </nav>
   );
 };
