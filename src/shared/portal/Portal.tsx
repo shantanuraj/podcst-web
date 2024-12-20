c00d8621d1480df06c7100021b3b60e257fa4758
Shantanu Raj <s@sraj.me>
2024-12-20 23:02:08 +0100

    fix(portal): Simplify usage, consolidate to component

--- src/shared/portal/Portal.tsx
+++ src/shared/portal/Portal.tsx
@@ -1,11 +1,21 @@
 'use client';
 
+import { useEffect, useRef, useState } from 'react';
 import { createPortal } from 'react-dom';
-import { usePortal } from './usePortal';
 
 type Props = {
   id: string;
   children: Parameters<typeof createPortal>[0];
 };
 
-export const Portal = ({ id, children }: Props) => createPortal(children, usePortal(id));
+export const Portal = ({ id, children }: Props) => {
+  const [mounted, setMounted] = useState(false);
+  const rootElemRef = useRef<Element | null>(null);
+
+  useEffect(() => {
+    setMounted(true);
+    rootElemRef.current = document.getElementById(id);
+  }, []);
+
+  return mounted && rootElemRef.current ? createPortal(children, rootElemRef.current) : null;
+};
--- src/shared/portal/index.ts
+++ src/shared/portal/index.ts
@@ -1,2 +1,1 @@
-export * from './usePortal';
 export * from './Portal';
