2cbc5d4bb666b2a8762b0d12784a0ca88931c56d
Shantanu Raj <s@sraj.me>
2025-12-21 02:24:31 +0100

    fix(search): Guard against API call for empty search term

--- src/data/search.ts
+++ src/data/search.ts
@@ -4,6 +4,9 @@ import { IPodcastSearchResult } from '@/types';
 import { get } from './api';
 
 export const searchTerm = async (term: string) => {
+  if (typeof term !== 'string' || term.trim().length === 0) {
+    return [];
+  }
   try {
     return get<IPodcastSearchResult[]>(`/search`, { term });
   } catch (err) {
