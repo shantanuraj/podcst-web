322c5b5aa93f6bbbfce3f275001326715871c6ba
Shantanu Raj <s@sraj.me>
2025-12-22 01:21:37 +0100

    fix(api): Use contextual api endpoint

--- src/data/api.ts
+++ src/data/api.ts
@@ -1,13 +1,30 @@
+function getBaseUrl() {
+  if (typeof window !== 'undefined') {
+    return '';
+  }
+  if (process.env.APP_URL) {
+    return process.env.APP_URL;
+  }
+  if (process.env.FLY_APP_NAME) {
+    return `https://${process.env.FLY_APP_NAME}.fly.dev`;
+  }
+  if (process.env.VERCEL_URL) {
+    return `https://${process.env.VERCEL_URL}`;
+  }
+  return 'http://localhost:3000';
+}
+
 export const get = async <T>(
   endpoint: string,
   params: Record<string, unknown>,
   revalidate?: number,
 ): Promise<T> => {
-  const url = new URL(`https://www.podcst.app/api${endpoint}`);
+  const searchParams = new URLSearchParams();
   Object.keys(params).forEach((key) => {
-    url.searchParams.append(key, params[key] as string);
+    searchParams.append(key, params[key] as string);
   });
-  const response = await fetch(url.toString(), {
+  const url = `${getBaseUrl()}/api${endpoint}?${searchParams.toString()}`;
+  const response = await fetch(url, {
     next: {
       revalidate,
     },
@@ -17,7 +34,7 @@ export const get = async <T>(
     const json = JSON.parse(text);
     return json as T;
   } catch (e) {
-    console.error('Parse exception', `Response: ${text}.`, url.toString(), response.status);
+    console.error('Parse exception', `Response: ${text}.`, url, response.status);
     throw e;
   }
 };
